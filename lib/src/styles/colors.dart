a0b5905d186f699dc54406f350e073f6f19dcead
jessica malak <jessimalak21@gmail.com>
2025-06-10 22:57:29 -0500

    add dark blue color

--- lib/src/styles/colors.dart
+++ lib/src/styles/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 class AquaColors {
   static const Color white = Color(0xffffffff);
   static const Color blue = Color.fromARGB(255, 0, 65, 184);
+  static const Color darkBlue = Color.fromARGB(255, 1, 34, 95);
   static const Color black = Color(0xff000000);
   static const Color lightGrey = Color(0xfff0f0f0);
 }
